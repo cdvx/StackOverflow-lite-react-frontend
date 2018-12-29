0445b14a1fb72cafade81b3dc3f7230f9dc690d4
cdvx <cdvx@fox.com>
2018-12-29 14:28:38 +0300

    [feature #162880270] Users can post questions

--- src/components/PostQuestion.jsx
+++ src/components/PostQuestion.jsx
@@ -2,30 +2,6 @@ import React, { Component } from 'react';
 import {connect} from "react-redux";
 import askQuestion from "../actions/postQuestionActions";
 
-
-// const QuestionModal = ({children}) => {
-//     return ( 
-//         <div class="modal">
-//         <div class="modal-dialog">
-//             <div class="modal-content">
-//             <div class="modal-header">
-//                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-//                 <h4 class="modal-title">Ask Question</h4>
-//             </div>
-//             <div class="modal-body">
-//                 {children}
-//             </div>
-//             <div class="modal-footer">
-//                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-//                 <button type="button" class="btn btn-primary">Save changes</button>
-//             </div>
-//             </div>
-//         </div>
-//         </div>
-//      );
-// }
-
-
 class PostQuestion extends Component {
     state = {  
         topic: "",
